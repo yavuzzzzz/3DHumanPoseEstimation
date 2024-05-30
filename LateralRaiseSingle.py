96612a2182689e3e5186f155b1840166d3d01ce5
malierel <malierel16@gmail.com>
2024-05-31 02:27:05 +0300

    çizgiler kaldırılıp omuz kasındaki renk değişimi eklendi

--- LateralRaiseSingle.py
+++ LateralRaiseSingle.py
@@ -54,48 +54,16 @@ while True:
             angle_text = "Angle: {} degrees".format(angle_deg)
 
             # Calculate color based on angle_deg
-            green = max(0, int((180 - angle_deg) / 180 * 255))
-            red = max(0, int((angle_deg / 180) * 255))
+            red = max(0, int((180 - angle_deg) / 180 * 255))
+            green = max(0, int((angle_deg / 180) * 255))
             color = (0, red, green)
 
-            percentage = round((angle_deg / 180) * 100)
-            cv2.putText(img, str(100 - percentage) + "%", (list[indices[1]][0], list[indices[1]][1] + 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
-            center = (b[0], b[1])
-
-            radius = 10
-            mask = np.zeros_like(img)
-            cv2.circle(mask, center, radius, (255, 255, 255), 5)
-
-            arc_mask = np.zeros_like(img)
-            start_angle = np.degrees(np.arctan2(ba[1], ba[0]))
-            end_angle = np.degrees(np.arctan2(bc[1], bc[0]))
-
-            if start_angle > end_angle:
-                start_angle, end_angle = end_angle, start_angle
-
-            if end_angle - start_angle > 180:
-                start_angle, end_angle = end_angle, start_angle + 360
-
-            cv2.ellipse(arc_mask, center, (radius, radius), 0, start_angle, end_angle, (255, 255, 255), 1)
-            arc = cv2.bitwise_and(mask, arc_mask)
-            img = cv2.bitwise_or(img, arc)
-
-            mp_draw.draw_landmarks(img, results.pose_landmarks, hip_to_shoulder_to_elbow,
-                                   mp_draw.DrawingSpec((0, 0, 0), 2, 1),
-                                   mp_draw.DrawingSpec(color, 5, 1))
+            # Draw colored circle at shoulder joint
+            cv2.circle(img, (b[0], b[1]), 10, color, -1)
 
     cv2.imshow("Pose Estimation", img)
 
-    h, w, c = img.shape
-    opImg = np.zeros([h, w, c])
-    mp_draw.draw_landmarks(opImg, results.pose_landmarks, hip_to_shoulder_to_elbow,
-                           mp_draw.DrawingSpec((255, 0, 0), 3, 3),
-                           mp_draw.DrawingSpec((0, 255, 0), 10, 3))
-
-    cv2.imshow("Extracted Pose", opImg)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
 cap.release()
