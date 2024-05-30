e7066263c137bf3e1836e12c1b9a7d2289ca8aee
Yavuz Yılmaz <74810831+yavuzzzzz@users.noreply.github.com>
2024-05-31 02:58:46 +0300

    Update model_check.py

--- model_check.py
+++ model_check.py
@@ -1,16 +1,37 @@
+import subprocess
 from tensorflow.keras.models import load_model
 import numpy as np
 
+# CNNImpl dosyasından X_train'i import edin
 from CNNImpl import X_train
+from Trigger import landmarks_file
 
 # Modeli yükle
 model = load_model('my_model.keras')
 
-# Girdi verisini yükle veya oluştur
-input_data = np.random.rand(1, X_train.shape[1], X_train.shape[2])
+def load_data_from_txt(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            points = line.strip().split()
+            points = [float(p) for p in points]
+            # Group every 3 elements and store in the list
+            if len(points) % 3 == 0:  # If the total number of points is divisible by 3
+                grouped_points = [points[i:i + 3] for i in range(0, len(points), 3)]
+                data.append(grouped_points)
+    return data
 
-# Modeli kullanarak tahmin yap
-predictions = model.predict(input_data)
+demo = load_data_from_txt(landmarks_file)
+demo = np.array(demo, dtype=np.float32)
 
-# Tahminleri yazdır
-print(predictions)
+# Reshape the data to match the expected input shape for Conv1D
+# The new shape is (1, number of frames, number of landmarks * 3)
+demo = demo.reshape(-1, X_train.shape[1], X_train.shape[2])
+
+# Make a prediction using the model
+probability = model.predict(demo)[0][0]
+# Trigger different Python files based on the prediction
+if probability > 0.5:
+    subprocess.call(["python", "BarbellBicepsCurlSingle.py"])
+else:
+    subprocess.call(["python", "LateralRaiseSingle.py"])
